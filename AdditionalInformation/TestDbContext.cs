30c3706eb2bf0c71da2b6465c102b0c70597431b
Asma <asmaa.abualkheir@gmail.com>
2023-11-24 09:14:32 +0100

    Add project files.

---
+++ AdditionalInformation/TestDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ClassLibrary1;
+using Microsoft.EntityFrameworkCore;
+
+namespace AdditionalInformation
+{
+    public class TestDbContext :DbContext
+    {
+        public TestDbContext(DbContextOptions<TestDbContext>options) : base(options) { }
+
+        public DbSet<UserManagement> Users { get; set; }
+    }
+}
